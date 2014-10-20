3648c2a97ab25a8b23b0d21303dbc3d0b90c398f
Evan Drewry <evan@vfiles.com>
2014-10-20 12:45:10 -0400

    add CNAME file, copy to dist build

--- Gruntfile.js
+++ Gruntfile.js
@@ -322,6 +322,7 @@ module.exports = function (grunt) {
             '*.{ico,png,txt}',
             '.htaccess',
             '*.html',
+            'CNAME',
             'views/{,*/}*.html',
             'images/{,*/}*.{webp}',
             'fonts/*'
